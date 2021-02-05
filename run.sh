ab06ac7da08a3c71f388d63b30a9d9f6b7923e8b
Jukka Svahn <jukka@lamia.fi>
2021-02-05 16:03:12 +0200

    Fixes space handling and POSIX compatibility
    Makes sure the script supports filepaths with spaces,
    updates error handling to use STDERR and EXIT code 1,
    and drops local bash dependency. Now any POSIX
    compliant shell should be able to execute it instead
    of just Bash.

--- run.sh
+++ run.sh
@@ -1,17 +1,16 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 NAME='markdown-to-pdf-with-plantuml'
 
-if [[ "$(docker images -q $NAME 2> /dev/null)" == "" ]]; then
-    docker build -t $NAME .
+if [ "$(docker images -q $NAME 2> /dev/null)" = "" ]; then
+    docker build -t $NAME . || exit 1
 fi
 
-DIRECTORY_TO_VOLUME=$1
+DIRECTORY_TO_VOLUME="$1"
 
-if [ -z "$DIRECTORY_TO_VOLUME" ]
-then
-    echo "Please provide path to the directory with files to build"
-    exit 0
+if [ -z "$DIRECTORY_TO_VOLUME" ]; then
+    echo "Please provide path to the directory with files to build" >&2
+    exit 1
 fi
 
-docker run -v $DIRECTORY_TO_VOLUME:/var/doc $NAME /var/build.sh
+docker run -v "$DIRECTORY_TO_VOLUME":/var/doc $NAME /var/build.sh || exit 1
